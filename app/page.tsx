c799ea263de19cecac7197f36e271fdb5bdd1e92
jake <jake.wmackie@gmail.com>
2024-10-24 00:55:06 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Who am I?
+        Jake Mackie — Fullstack Developer
       </h1>
       <span className="mb-4">
         My name is Jake Mackie and I am a 19 year old from the UK. Currently I
@@ -17,7 +17,10 @@ export default function Page() {
           Hiyield
         </Link>{" "}
         where I contribute to creating bespoke websites as part of the WordPress 
-        team.
+        team. I like to consider my time building websites as me doibg something
+        I love, which is why I made this one. Here you can find out all about what
+        I get up to, whether it's tech-related or personal, with a specfic section
+        to showcase projects.
       </span>
     </div>
   );
