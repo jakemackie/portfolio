c1e1d46c66dd8a381837723de80e50a575f061c3
jake <jake.wmackie@gmail.com>
2024-10-29 23:16:15 +0000

    Update technology.tsx

--- app/components/technology.tsx
+++ app/components/technology.tsx
@@ -15,7 +15,7 @@ export default function Technology({
 }: TechnologyProps) {
   return (
     <div className="w-full flex gap-3 bg-zinc-950 border border-neutral-800 p-2 rounded-xl hover:bg-zinc-900 hover:border-neutral-700 duration-200 ease-in-out">
-      <div className={`p-2 ${highlight} rounded-xl overflow-hidden`}>
+      <div className={`p-2 ${highlight} rounded-md overflow-hidden`}>
         <Image
           src={icon}
           alt={name}
