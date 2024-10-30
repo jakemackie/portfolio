ffa44813b7c86ced3ced73280d3c5c63c1dab685
jakemackie <jake.wmackie@gmail.com>
2024-10-30 22:57:18 +0000

    feat: Cool ogImage

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -8,17 +8,23 @@ export function GET(request: Request) {
   return new ImageResponse(
     (
       <div tw="flex flex-col w-full h-full items-center justify-center bg-[#0c0c0c] text-[#e2e8f0]">
-        <div tw="flex flex-col w-full py-12 px-4 justify-between p-38">
-          <h2 tw="text-6xl font-medium tracking-tight text-left">
-            Jake Mackie — Blog
-          </h2>
-          <h1 tw="text-8xl font-bold tracking-tight text-left">{title}</h1>
+        <h3 tw="absolute top-10 left-10 text-xl font-semibold">
+          jakemackie.co.uk
+        </h3>
+        <div tw="flex flex-col">
+          <div tw="relative mx-auto flex flex-col max-w-xl p-10 tracking-tight text-center">
+            <h1 tw="mx-auto text-5xl font-semibold">{title}</h1>
+            <p tw="absolute top-28 mx-auto text-xl text-neutral-400">
+              {`Find out more about the ${title} post on my personal
+              blog.`}
+            </p>
+          </div>
         </div>
       </div>
     ),
     {
-      width: 1200,
-      height: 630,
+      width: 800,
+      height: 400,
     }
   );
 }
