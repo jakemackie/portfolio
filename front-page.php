c59bb27d2205dc45645d3aa521124f24a7c74ae1
Jake Mackie <jake.wmackie@gmail.com>
2026-02-12 00:35:33 +0000

    feat: refining the admin area

--- front-page.php
+++ front-page.php
@@ -3,9 +3,7 @@
 <main id="main" class="site-main">
     <?php while ( have_posts() ) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <div class="entry-content">
-                <?php the_content(); ?>
-            </div>
+            <?php the_content(); ?>
         </article>
     <?php endwhile; ?>
 </main>
