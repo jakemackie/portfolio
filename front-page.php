7813455625e27ac5e4189faec6bd941e28f1b4e5
Jake Mackie <jake.wmackie@gmail.com>
2026-02-14 18:15:46 +0000

    amend: updates to the header

--- front-page.php
+++ front-page.php
@@ -4,6 +4,10 @@
     <?php while ( have_posts() ) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <?php the_content(); ?>
+            <div class="flex justify-end bg-blue-500 min-h-screen">
+                test
+            </div>
+
         </article>
     <?php endwhile; ?>
 </main>
--- template-parts/global/header.php
+++ template-parts/global/header.php
@@ -1,12 +1,19 @@
 <header>
     <section class="container mx-auto grid-design">
-        <div class="col-span-full flex justify-between py-6">
-            <div>
+        <div class="relative col-span-full flex justify-between py-6">
+            <div class="hidden lg:block">
                 <h3 class="text-2xl">Jake Mackie</h3>
             </div>
 
-            <ul class="flex *:hover:bg-gray-50 *:transition-colors *:duration-200 *:ease-in-out">
-                <li class="flex items-center bg-gray-100 rounded-l-full">
+            <ul class="<?php echo(is_admin_bar_showing() ? 'top-11' : 'top-3'); ?> 
+                fixed left-1/2 -translate-x-1/2 z-50
+                flex bg-gray-100/50 backdrop-blur-2xl *:hover:bg-gray-200/70 
+                rounded-full border border-gray-300/50 overflow-hidden 
+                *:transition-colors *:duration-300 *:ease-in-out
+                lg:left-auto lg:right-[max(1rem,calc((100vw-1280px)/2))]
+                lg:translate-x-0
+            ">
+                <li>
                     <a 
                         href="#"
                         class="flex items-center py-4 pl-6 pr-4"
@@ -16,18 +23,19 @@
                     </a>
                 </li>
 
-                <li class="flex items-center bg-gray-100">
+                <li>
                     <a 
                         href="#"
-                        class="p-4"
+                        class="flex items-center p-4"
                     >
                         About
                     </a>
                 </li>
-                <li class="flex items-center bg-gray-100 rounded-r-full">
+
+                <li>
                     <a 
                         href="#"
-                        class="py-4 pl-4 pr-6"
+                        class="flex items-center py-4 pl-4 pr-6"
                     >
                         Contact
                     </a>
