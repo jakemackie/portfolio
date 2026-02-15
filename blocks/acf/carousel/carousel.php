c36b30a4e636995edaa9782ea0e46746652a68b9
Jake Mackie <jake@hiyield.co.uk>
2026-02-15 16:42:53 +0000

    amend: fix container

--- blocks/acf/carousel/carousel.php
+++ blocks/acf/carousel/carousel.php
@@ -33,25 +33,27 @@ if (empty($image_ids)) {
     <?php else : ?>
 
         <!-- Frontend Swiper -->
-        <div class="container swiper js-carousel">
-            <div class="swiper-wrapper">
-                <?php foreach ($image_ids as $image_id) : ?>
-                    <div class="swiper-slide aspect-3/2 rounded-2xl overflow-hidden">
-                        <?php
-                        echo wp_get_attachment_image(
-                            attachment_id: $image_id,
-                            size: 'large',
-                            attr: [
-                                'class' => 'size-full object-cover',
-                                'loading' => 'lazy'
-                            ]
-                        );
-                        ?>
-                    </div>
-                <?php endforeach; ?>
-            </div>
+        <div class="container">
+            <div class="swiper js-carousel">
+                <div class="swiper-wrapper">
+                    <?php foreach ($image_ids as $image_id) : ?>
+                        <div class="swiper-slide aspect-3/2 rounded-2xl overflow-hidden">
+                            <?php
+                            echo wp_get_attachment_image(
+                                attachment_id: $image_id,
+                                size: 'large',
+                                attr: [
+                                    'class' => 'size-full object-cover',
+                                    'loading' => 'lazy'
+                                ]
+                            );
+                            ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
 
-            <div class="swiper-pagination"></div>
+                <div class="swiper-pagination"></div>
+            </div>
         </div>
 
     <?php endif; ?>
